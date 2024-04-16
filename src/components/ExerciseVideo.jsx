436d32b47ba296afd4d2d92f94cbd338017361b5
VannestBun <114324735+VannestBun@users.noreply.github.com>
2024-04-16 16:49:27 +1000

    make the youtube videos thumbnails consistent size

--- src/components/ExerciseVideo.jsx
+++ src/components/ExerciseVideo.jsx
@@ -48,15 +48,15 @@ export default function ExerciseVideo(props) {
     return (
         <div>
             <h3 className='text-3xl text-center font-medium'>Watch {props.name} Exercise Videos!</h3>
-            <div className='flex flex-wrap gap-4 mx-0 mt-20'>
+            <div className='flex flex-wrap gap-6 mx-0 mt-20'>
                 {exerciseVideos.slice(0, 6).map((exercise, index) => (
-                    <div key={index} className='flex flex-col items-center mx-auto'>
+                    <div key={index} className='flex flex-col items-center mx-auto w-96 text-center'>
                         <a
                             href={`https://www.youtube.com/watch?v=${exercise.video.videoId}`}
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <img src={exercise.video.thumbnails[0].url} alt={exercise.video.title} />
+                            <img src={exercise.video.thumbnails[0].url} alt={exercise.video.title} className="w-80" />
                         </a>
                         <p className='mt-2'>{exercise.video.title}</p>
                         <p className='text-gray-500'>{exercise.video.channelName}</p>
